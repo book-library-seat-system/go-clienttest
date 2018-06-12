dca49eee01d0ea44f68ca69ed17e26db7df5a323
blingbling0211 <38324134+blingbling0211@users.noreply.github.com>
2018-06-12 12:22:56 +0800

    Add files via upload

--- gotest_test.go
+++ gotest_test.go
@@ -7,7 +7,14 @@ import (
 )
 
 // UserHandle 测试部分
-func TestUserHandle(t *testing.T) {
-	server.TesttestGET(t)
-	server.TesttestPost(t)
+// func TestUserHandle(t *testing.T) {
+// 	server.TesttestGET(t)
+// 	server.TesttestPost(t)
+// }
+
+// UserHandle 测试部分
+func TestSeatHandle(t *testing.T) {
+	server.TestShowTimeIntervalInfoHandle(t)
+	server.TestShowSeatInfoHandle1(t)
+	server.TestShowSeatInfoHandle2(t)
 }
